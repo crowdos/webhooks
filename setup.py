126fc0c85308ff83d7306aa01db989d9d495e033
Mohammed Sameer <msameer@foolab.org>
2016-07-23 17:52:52 +0300

    setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(name='webhooks tools',
+      version='1.0',
+      packages=['webhooks', 'webhooks.admin', 'webhooks.hooks', 'webhooks.obs'],
+      scripts=['webhooks.py'],
+      data_files=[('/etc/webhooks/', ['conf.json'])]
+      )
